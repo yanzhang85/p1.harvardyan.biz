4e8e9b6b4473fddaaadd2707199a73f69589cacf
Yan Zhang <yanzhang01@g.harvard.edu>
2013-09-17 01:51:43 -0700

    Adding main index page

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
- <style>
+ <style type="text/css">
   	.last {
  	 line-height:0.5px;
 	}
@@ -14,18 +14,18 @@
 	<body>
 	    <div align="center"> <h1><strong> <font color="purple">Welcome to Yan's house!</font> </strong> </h1></div>
 
-        <br />
+        <br >
 
 		<p> <img src="http://m.c.lnkd.licdn.com/mpr/mpr/shrink_200_200/p/8/000/1d7/114/214ecbc.jpg" vspace="10" hspace="15" width="120" height="120" alt="Image not available.." align="right">
 		<font face="Times New Roman"> Hello, my friends!</p> 
 
 		<p> I am <font color="blue"><i>Yan Zhang </font></i>, a 28-year-old guy from Shanghai, China. In 2007, I came to US to study biology, and earlier this year I earned the PhD in Genetics. Currently I am working in a bio-tech company. One of my projects is to promote our products online, which brings me to this class. I am interested in computer science, and I think this course will be helpful to my job and future career. In addition, I passed Charted Financial Analyst level III exam this year. I love to make friends, and I hope we all have good time here.</font></p>
 
-	     <br /><br /><br />
+	     <br ><br ><br >
          <p> <font face="Times New Roman">More about me on <a href="https://www.facebook.com/yeah.zhang" target="_blank"><img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQglnj22bovLpOaua89GaTjmOTrPLZdLgNBNU2xJxDkssZfJkPu" width="18" height="16" alt="Image not available.."></a>.. </font></p>
          
-	     <br /><br /><br />
-	     <hr />
+	     <br ><br ><br >
+	     <hr >
 	     <p class="last"> <tt><font face="Comic sans MS" size=1> Edited in Sublime Text2</font> </tt></p></div>
 	     
 	</body>
